d8673f8c1f23d228012769eb2640adb677b69e36
David <wdturnbo@uark.edu>
2020-09-19 00:35:06 -0500

    Number of Unique Paths correct answer

--- Geeks For Geeks/Number Of Unique Paths/Number Of Unique Paths/src/number/of/unique/paths/NumberOfUniquePaths.java
+++ Geeks For Geeks/Number Of Unique Paths/Number Of Unique Paths/src/number/of/unique/paths/NumberOfUniquePaths.java
@@ -6,43 +6,55 @@
 //******************************************************************************
 package number.of.unique.paths;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class NumberOfUniquePaths
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        Scanner in = new Scanner(System.in);
-
-        int testCases = in.nextInt();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        int testCases = Integer.parseInt(in.readLine());
 
         while (testCases-- > 0)
         {
-            int rows = in.nextInt();
-            int cols = in.nextInt();
-
+            String[] sizes = in.readLine().split(" ");
+            
+            int rows = Integer.parseInt(sizes[0]);
+            int cols = Integer.parseInt(sizes[1]);
+            
             int[][] arr = new int[rows][cols];
-
-            for (int i = 0; i < rows; ++i)
+            
+            // Initialize the first row. Only one unique way of reaching each
+            //  square.
+            for(int i = 0; i < rows; ++i)
             {
                 arr[i][0] = 1;
             }
-
-            for (int i = 0; i < cols; ++i)
+            
+            // Initialize the first col. Only one unique way of reaching each
+            //  square.
+            for(int i = 0; i < cols; ++i)
             {
                 arr[0][i] = 1;
             }
-
-            for (int i = 1; i < rows; ++i)
+            
+            // Fill ine the remaining squares, with the number of unique ways
+            //   of reaching them, until the end is reached.
+            for(int i = 1; i < rows; ++i)
             {
-                for (int j = 1; j < cols; ++j)
+                for(int j = 1; j < cols; ++j)
                 {
                     arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
                 }
             }
-
+            
+            // Print the final square, bottom right.
             System.out.printf("%d%n", arr[rows - 1][cols - 1]);
+            
         }
     }
 }
