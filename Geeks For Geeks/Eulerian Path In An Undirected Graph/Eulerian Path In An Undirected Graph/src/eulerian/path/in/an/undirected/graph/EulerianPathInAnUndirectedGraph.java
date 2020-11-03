2ae8a045e43352e0044674df14f59a73e179f069
David <wdturnbo@uark.edu>
2020-11-03 12:58:21 -0600

    Program takes the required input now

--- Geeks For Geeks/Eulerian Path In An Undirected Graph/Eulerian Path In An Undirected Graph/src/eulerian/path/in/an/undirected/graph/EulerianPathInAnUndirectedGraph.java
+++ Geeks For Geeks/Eulerian Path In An Undirected Graph/Eulerian Path In An Undirected Graph/src/eulerian/path/in/an/undirected/graph/EulerianPathInAnUndirectedGraph.java
@@ -1,23 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Name: David Turnbough
+// Date: Tuesday November 3, 2020
+// Geeks For Geeks: Eulerian Path In An Undirected Graph
+// https://practice.geeksforgeeks.org/problems/eulerian-path-in-an-undirected-graph/0/?problemStatus=unsolved&problemType=full&difficulty[]=0&page=1&query=problemStatusunsolvedproblemTypefulldifficulty[]0page1
+
 package eulerian.path.in.an.undirected.graph;
 
-/**
- *
- * @author JR
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class EulerianPathInAnUndirectedGraph
 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        // TODO code application logic here
+        // Generate an input stream
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Get the number of test cases
+        int testCases = Integer.parseInt(in.readLine());
+        
+        // Loop through the test cases
+        while(testCases-- > 0)
+        {
+            // Get the total number of vertices in the graph
+            int vertices = Integer.parseInt(in.readLine());
+            
+            // Create a graph matrix to contain the edges between vertices
+            int[][] graph = new int[vertices][vertices];
+            
+            // Get the edges between vertices and store them in the graph
+            for(int i = 0; i < vertices; ++i)
+            {
+                String[] rawInput = in.readLine().split(" ");
+                
+                for(int j = 0; j < vertices; ++j)
+                {
+                    graph[i][j] = Integer.parseInt(rawInput[j]);
+                }
+            }
+        }
     }
-    
 }
